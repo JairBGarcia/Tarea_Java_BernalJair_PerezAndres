c0ca197fc2e6e6801317318117db06c76eed4348
JairGarcia <156432366+JairBGarcia@users.noreply.github.com>
2024-05-23 18:05:31 -0500

    Add files via upload

---
+++ Sobrecarga/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sobrecarga;
+
+/**
+ *
+ * @author jairb
+ */
+public class Calculadora {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public String concatenar(String a, String b) {
+        return a + b;
+    }
+}
+
