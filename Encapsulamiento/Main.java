8c4f54534f777c6f1cc951410e7aa49c40e2f338
JairGarcia <156432366+JairBGarcia@users.noreply.github.com>
2024-05-27 07:28:05 -0500

    feat :sparkles: chismosin

--- Encapsulamiento/Main.java
+++ Encapsulamiento/Main.java
@@ -8,9 +8,45 @@ package Encapsulamiento;
  *
  * @author camper
  */
-public class Main {
+public class JavaApplication4 {
+    private String marca;
+    private double diametro;
+    private String color;
+    private boolean inflado;
+
+    public JavaApplication4(String marca, double diametro, String color) {
+        this.marca = marca;
+        this.diametro = diametro;
+        this.color = color;
+        this.inflado = false; // Al inicio el balón está desinflado
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getDiametro() {
+        return diametro;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void inflar() {
+        this.inflado = true;
+    }
+
+    public void desinflar() {
+        this.inflado = false;
+    }
+
+    public boolean estaInflado() {
+        return inflado;
+    }
+
     public static void main(String[] args) {
-        Balon balon = new Balon("Nike", 30.5, "Rojo");
+        JavaApplication4 balon = new JavaApplication4("Nike", 30.5, "Rojo");
 
         System.out.println("Marca del balon: " + balon.getMarca());
         System.out.println("Diámetro del balon: " + balon.getDiametro());
